aa43bc762c6acc04aabad51d87c7601b2e477009
yhtham1 <yhtham1@m1.pxaa.net>
2021-03-07 13:17:26 +0900

    aaa

--- boot.py
+++ boot.py
@@ -1,1 +1,16 @@
-# This file is executed on every boot (including wake-boot from deepsleep)#import esp#esp.osdebug(None)#import webrepl#webrepl.start()import pwm27import rgm24g
+
+
+# This file is executed on every boot (including wake-boot from deepsleep)
+
+#import esp
+
+#esp.osdebug(None)
+
+#import webrepl
+
+#webrepl.start()
+
+import pwm27
+
+import rgm24g
+
